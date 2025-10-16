992b57b21efc2e0c1af3d99739761f153e497e7c
Fernandouu <20190369@ricaldone.edu.sv>
2025-10-16 12:06:35 -0600

    cambio de validacion

--- src/middlewares/scheme/horariosScheme.js
+++ src/middlewares/scheme/horariosScheme.js
@@ -3,11 +3,3 @@ import Joi from "joi";
 export const DiasSemana = Joi.object({
     dia_semana: Joi.string().required(),
 })
-
-export const HorarioDiaSemana = Joi.object({
-    id_horario: Joi.number().required(),
-    id_dia_semana: Joi.number().required(),
-    activo: Joi.number().required(),
-    fecha_inicio_validez: Joi.date().required(),
-    fecha_fin_validez: Joi.date().required(),
-})
--- src/rutas/horarioDiaSemana.routes.js
+++ src/rutas/horarioDiaSemana.routes.js
@@ -1,14 +1,12 @@
 import {Router } from "express";
 import {getHorarioDiaSemana,getHorarioDiaSemanaId,updateHorarioDiaSemana,insertHorarioDiaSemana,deleteHorarioDiaSemana} from "../controladores/horarioDiaSemana.js"
-import { validate } from "../middlewares/validacion.js";
-import { HorarioDiaSemana } from "../middlewares/scheme/horariosScheme.js";
 import {verificarToken, verificarNivel} from "../middlewares/authMiddleware.js"
 const router = Router()
 
 router.get("/GEThorarioDiaSemana", verificarToken, verificarNivel(['admin', 'profesor']), getHorarioDiaSemana)
 router.get("/GEThorarioDiaSemana/:id", verificarToken, verificarNivel(['admin', 'profesor']), getHorarioDiaSemanaId)
-router.put("/PUThorarioDiaSemana/:id", verificarToken, verificarNivel(['admin', 'profesor']), validate(HorarioDiaSemana), updateHorarioDiaSemana)
-router.post("/POSThorarioDiaSemana", verificarToken, verificarNivel(['admin', 'profesor']), validate(HorarioDiaSemana), insertHorarioDiaSemana)
+router.put("/PUThorarioDiaSemana/:id", verificarToken, verificarNivel(['admin', 'profesor']), updateHorarioDiaSemana)
+router.post("/POSThorarioDiaSemana", verificarToken, verificarNivel(['admin', 'profesor']), insertHorarioDiaSemana)
 router.delete("/DELETEhorarioDiaSemana/:id", verificarToken, verificarNivel(['admin', 'profesor']), deleteHorarioDiaSemana)
 
 export default router
